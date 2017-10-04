63ec3b9cfd342f812e4e32037feb612a89de522c
Sidney San Martín <sidney@s4y.us>
2017-10-03 22:39:04 -0400

    WIP

--- halcyon.c
+++ halcyon.c
@@ -1,10 +1,9 @@
+#include <asm/termbits.h>
 #include <bcm2835.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <linux/serial.h>
 #include <stdio.h>
 #include <sys/ioctl.h>
-#include <termios.h>
 #include <unistd.h>
 
 #define SERIAL_PORT "/dev/ttyAMA0"
@@ -25,59 +24,31 @@ int main() {
 
 	int rx = open(SERIAL_PORT, O_RDWR | O_NOCTTY);
 
-    struct serial_struct serinfo = {0};
+	// https://stackoverflow.com/a/19992472/84745
+	struct termios2 tio;
 
-	if (ioctl(rx, TIOCGSERIAL, &serinfo) < 0) {
-		fprintf(stderr, "TIOCGSERIAL: %d", errno);
-		return 1;
-	}
+	ioctl(rx, TCGETS2, &tio);
+	tio.c_cflag &= ~(CBAUD | CSIZE);
+	tio.c_cflag |= CS8|PARENB|PARODD|BOTHER;
+	tio.c_lflag &= ~ICANON;
 
-	serinfo.flags &= ~ASYNC_SPD_MASK;
-	serinfo.flags |= ASYNC_SPD_CUST;
-	serinfo.custom_divisor = SERIAL_DIVISOR;
+	tio.c_ispeed = 500;
+	tio.c_ospeed = 500;
 
-	if (ioctl(rx, TIOCSSERIAL, &serinfo) < 0) {
-		fprintf(stderr, "TIOCSSERIAL: %d", errno);
-		return 1;
-	}
+	tio.c_cc[VMIN] = 16;
+	tio.c_cc[VTIME] = 5;
 
-	if (ioctl(rx, TIOCGSERIAL, &serinfo) < 0) {
-		fprintf(stderr, "TIOCGSERIAL: %d", errno);
-		return 1;
-	}
-
-	printf("rchar: %d, baud_base: %d, custom_divisor: %d\n", serinfo.reserved_char, serinfo.baud_base, serinfo.custom_divisor);
-
-	struct termios tty = {0};
-
-	if (tcgetattr(rx, &tty)) {
-		fprintf(stderr, "tcgetattr: %d", errno);
-		return 1;
-	}
-
-	cfsetospeed(&tty, B38400);
-	cfsetispeed(&tty, B38400);
-	cfmakeraw(&tty);
-
-	tty.c_cflag &= ~CRTSCTS;
-	tty.c_cflag |= CS7|PARENB|PARODD|CREAD|CLOCAL;
-
-	printf("c_cflag %d\n", tty.c_cflag);
-	printf("vtime was: %d\n", tty.c_cc[VTIME]);
-	tty.c_cc[VMIN] = 16;
-	tty.c_cc[VTIME] = 2;
-
-	if (tcsetattr(rx, TCSANOW, &tty)) {
-		fprintf(stderr, "tcsetattr: %d", errno);
+	if (ioctl(rx, TCSETS2, &tio)) {
 		return 1;
 	}
 
 	unsigned char buf[256];
 	ssize_t len;
 	while ((len = read(rx, buf, sizeof(buf) / sizeof(*buf))) > 0) {
-		printf("read %d bytes\n", len);
-		for (size_t i = 0; i < len; i++)
+		for (size_t i = 0; i < len; i++) {
+			buf[i] = ~buf[i];
 			printf("%02x ", buf[i]);
+		}
 		printf("\n");
 	}
 }
