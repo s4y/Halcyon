7d71ec083d493687bb3673da450384aac61c8104
Sidney San Martín <sidney@s4y.us>
2017-12-25 10:00:51 -0500

    Exciting times. We can upload, debug, and use the softdevice.

--- halcyon.c
+++ halcyon.c
@@ -6,7 +6,6 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <time.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <signal.h>
@@ -15,13 +14,6 @@
 #define SERIAL_DIVISOR 6000
 
 #define LISTEN_PORT 9972
-
-uint64_t ustime() {
-	struct timespec time;
-	if (clock_gettime(CLOCK_MONOTONIC, &time) != 0)
-		return errno;
-	return (time.tv_sec * 1000000) + (time.tv_nsec / 1000);
-}
 
 int openserial() {
 	int ret = open(SERIAL_PORT, O_RDWR | O_NOCTTY);
@@ -55,7 +47,9 @@ int main() {
 		addr.sin_family = AF_INET;
 		addr.sin_addr.s_addr = htonl(INADDR_ANY);
 		addr.sin_port = htons(LISTEN_PORT);
-		bind(lsock, (struct sockaddr *)&addr, sizeof(addr));
+		setsockopt(lsock, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+		if (bind(lsock, (struct sockaddr *)&addr, sizeof(addr)))
+			return 2;
 	}
 	listen(lsock, 0);
 
@@ -65,11 +59,67 @@ int main() {
 		if (serial < 0)
 			return 1;
 
+		unsigned char ceil_buf[7] = {0};
+		unsigned char therm_buf[7] = {0};
+		unsigned char our_buf[8] = {
+			(uint8_t)~0x21,
+			(uint8_t)~0x81,
+			(uint8_t)~0x00,
+			(uint8_t)~0x00,
+			(uint8_t)~0x00,
+			(uint8_t)~0x20,
+			(uint8_t)~0x00,
+			(uint8_t)~0x00,
+		};
+
 		unsigned char buf[64];
 		ssize_t len;
 		while ((len = read(serial, buf, sizeof(buf) / sizeof(*buf))) > 0) {
-			dprintf(rsock, "%lld ", ustime());
-			for (size_t i = 0; i < len; i++) {
+			unsigned char* known_buf = 0;
+			if (len == 8) {
+				switch (buf[0]) {
+					case (uint8_t)~0x00:
+						known_buf = ceil_buf;
+						break;
+					case (uint8_t)~0x20:
+						known_buf = therm_buf;
+						break;
+					default:
+						break;
+				}
+			}
+			if (known_buf) {
+				int changed = 0;
+				for (ssize_t i = 1; i < len; i++) {
+					if (buf[i] != known_buf[i-1]) {
+						changed = 1;
+						known_buf[i-1] = buf[i];
+					}
+				}
+				if (buf[0] == (uint8_t)~0x20) {
+					fprintf(stderr, "copy time....\n");
+					for (size_t i = 1; i < (sizeof(our_buf) / sizeof(*our_buf)); i++) {
+						our_buf[i] = known_buf[i-1];
+					}
+					our_buf[1] = (uint8_t)~0x81;
+					//our_buf[2] = (uint8_t)~0x00;
+					our_buf[5] = ceil_buf[2] == (uint8_t)~0x10 ? 0xff : (uint8_t)~0x20;
+					fprintf(stderr, "write wait...\n");
+					usleep(10000);
+					fprintf(stderr, "write time.\n");
+					int wrote;
+					if ((wrote = write(serial, our_buf, sizeof(our_buf))) > 0) {
+						fprintf(stderr, "write good: %d\n", wrote);
+					} else {
+						fprintf(stderr, "write bad: %d\n", errno);
+					}
+				}
+
+				if (!changed)
+					continue;
+			}
+
+			for (ssize_t i = 0; i < len; i++) {
 				buf[i] = ~buf[i];
 				dprintf(rsock, "%02x ", buf[i]);
 			}
