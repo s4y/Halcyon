65310a4a069b2ff57d906986138de95d5bffb680
Sidney San Martín <sidney@s4y.us>
2017-12-30 01:36:45 -0500

    We can see what's up.

--- redbear/mbed/halcyon/source/main.cpp
+++ redbear/mbed/halcyon/source/main.cpp
@@ -1,39 +1,23 @@
 #include "mbed.h"
 #include "BLE.h"
 
+#include <array>
+
 const char* const kDeviceName = "Halcyon bridge";
-
-uint8_t serialBuf[8];
-
-uint8_t ceilBuf[7];
-uint8_t thermBuf[7];
 
 DigitalOut led(LED1, 0);
 
-#if 0
-GattCharacteristic bufCharacteristic(0xb00f, ceilBuf, sizeof(serialBuf), sizeof(serialBuf), GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, NULL, 0, false);
-GattCharacteristic ceilCharacteristic(0xACff, ceilBuf, sizeof(ceilBuf), sizeof(ceilBuf), GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, NULL, 0, false);
-GattCharacteristic thermCharacteristic(0xACdf, thermBuf, sizeof(thermBuf), sizeof(thermBuf), GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, NULL, 0, false);
-
-GattCharacteristic* characteristics[] = {
-	&bufCharacteristic,
-	&ceilCharacteristic,
-	&thermCharacteristic,
-};
-GattService acService(0xACAC, characteristics, sizeof(characteristics) / sizeof(GattCharacteristic *));
-#endif
-
 class HalcyonBus {
 	public:
-	enum Node {
+	enum class Node {
 		Blower,
 		PrimaryRemote,
 		SecondaryRemote,
 	};
 
 	struct DebugObserver {
-		virtual void didReadPacket(const uint8_t* buf) {}
-		virtual void nodeDidChangeState(Node node, const uint8_t* buf) {}
+		virtual void didReadPacket(const std::array<uint8_t, 8>& buf) {}
+		virtual void nodeDidChangeState(Node node, const std::array<uint8_t, 7>& buf) {}
 	};
 
 	DebugObserver* observer = nullptr;
@@ -42,29 +26,49 @@ class HalcyonBus {
 	Serial uart;
 	Timeout timeout;
 
-	uint8_t serialBuf[8];
+	std::array<uint8_t, 8> serialBuf{{0}};
 
 	void handleRead(int) {
 		led = 0;
+		timeout.detach();
 		scheduleRead();
-
 		if (observer) observer->didReadPacket(serialBuf);
+
+		std::array<uint8_t, 7> stateBuf;
+		for (size_t i = 0; i < stateBuf.size(); i++)
+			stateBuf[i] = ~serialBuf[i+1];
+
+		switch (static_cast<uint8_t>(~serialBuf[0])) {
+			case 0x00:
+				if (observer) observer->nodeDidChangeState(Node::Blower, stateBuf);
+				break;
+			case 0x20:
+				if (observer) observer->nodeDidChangeState(Node::PrimaryRemote, stateBuf);
+				break;
+			default:
+				led = 1;
+				break;
+		}
 	}
 
 	void handleTimeout() {
-		led = !led;
+		led = 1;
 		uart.abort_read();
 		scheduleRead();
 	}
 
+	void handleReadStart() {
+		uart.attach(nullptr);
+		uart.read(serialBuf.data(), serialBuf.size(), callback(this, &HalcyonBus::handleRead));
+		timeout.attach(callback(this, &HalcyonBus::handleTimeout), 0.2);
+	}
+
 	void scheduleRead() {
-		timeout.detach();
-		uart.read(serialBuf, sizeof(serialBuf), callback(this, &HalcyonBus::handleRead));
-		timeout.attach(callback(this, &HalcyonBus::handleTimeout), 0.5);
+		uart.attach(callback(this, &HalcyonBus::handleReadStart));
 	}
 
 	public:
-	HalcyonBus(PinName rx, PinName tx) : uart(rx, tx, 1200) {
+	HalcyonBus(PinName tx, PinName rx) : uart(tx, rx, 500) {
 		// Even parity.
 		uart.format(8, SerialBase::Even);
 
@@ -80,29 +84,71 @@ class HalcyonBus {
 };
 
 class BLEService: HalcyonBus::DebugObserver {
-	GattCharacteristic bufCharacteristic{0xb00f, NULL, 0, 0, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, NULL, 0, false};
-	GattCharacteristic ceilCharacteristic{0xACff, NULL, 0, 0, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, NULL, 0, false};
-	GattCharacteristic thermCharacteristic{0xACdf, NULL, 0, 0, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, NULL, 0, false};
+
+	template <size_t Size>
+	class ROBufferCharacteristic {
+		std::array<uint8_t, Size> buf{{0}};
+		GattCharacteristic characteristic{0x0, buf.data(), buf.size(), buf.size(), GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, NULL, 0, false};
+
+		public:
+		ROBufferCharacteristic(const UUID uuid) :
+			characteristic{uuid, buf.data(), Size, Size, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY, NULL, 0, false}
+		{}
+
+		void update(const std::array<uint8_t, Size>& from) {
+			// TODO: Move this higher up the chain.
+			if (buf == from)
+				return;
+
+			buf = from;
+			BLE::Instance().gattServer().write(
+				characteristic.getValueAttribute().getHandle(), buf.data(), buf.size()
+			);
+		}
+
+		operator GattCharacteristic*() { return &characteristic; }
+	};
+
+	ROBufferCharacteristic<8> bufCharacteristic{0xb00f};
+	ROBufferCharacteristic<7> blowerCharacteristic{0xac00};
+	ROBufferCharacteristic<7> themostatCharacteristic{0xac20};
 
 	GattService service{[&]{
 		GattCharacteristic* characteristics[] = {
-			&bufCharacteristic,
-			&ceilCharacteristic,
-			&thermCharacteristic,
+			bufCharacteristic,
+			blowerCharacteristic,
+			themostatCharacteristic,
 		};
 		return GattService{0xACAC, characteristics, sizeof(characteristics) / sizeof(GattCharacteristic *)};
 	}()};
 
-	void didReadPacket(const uint8_t* buf) override {
-		BLE::Instance().updateCharacteristicValue(bufCharacteristic.getValueAttribute().getHandle(), serialBuf, 8);
+	void didReadPacket(const std::array<uint8_t, 8>& buf) override {
+		bufCharacteristic.update(buf);
+	}
+
+	void nodeDidChangeState(HalcyonBus::Node node, const std::array<uint8_t, 7>& buf) override {
+		switch (node) {
+			case HalcyonBus::Node::Blower:
+				blowerCharacteristic.update(buf);
+				break;
+			case HalcyonBus::Node::PrimaryRemote:
+				themostatCharacteristic.update(buf);
+				break;
+			default:
+				break;
+		}
+	}
+
+	void didDisconnect(const Gap::DisconnectionCallbackParams_t*) {
+		BLE::Instance().gap().startAdvertising();
 	}
 
 	public:
 	BLEService(HalcyonBus* bridge) {
 		BLE &ble = BLE::Instance();
 
-		// ble.gap().onConnection(&onConnection);
-		// ble.gap().onDisconnection(&onDisconnection);
+		//ble.gap().onConnection(&onConnection);
+		ble.gap().onDisconnection(this, &BLEService::didDisconnect);
 
 		ble.gattServer().addService(service);
 		ble.gap().setDeviceName(reinterpret_cast<const uint8_t*>(kDeviceName));
@@ -113,94 +159,7 @@ class BLEService: HalcyonBus::DebugObserver {
 	}
 };
 
-#if 0
-
-void scheduleRead();
-
-void onRead(int) {
-	scheduleRead();
-
-	uint8_t* targetBuf = NULL;
-	GattCharacteristic* targetCharacteristic = NULL;
-
-	for (size_t i = 1; i < sizeof(serialBuf) / sizeof(*serialBuf); i++)
-		serialBuf[i] = ~serialBuf[i];
-	BLE::Instance().updateCharacteristicValue(bufCharacteristic.getValueAttribute().getHandle(), serialBuf, sizeof(serialBuf));
-
-	switch (serialBuf[0]) {
-		case 0xff:
-			targetBuf = ceilBuf;
-			targetCharacteristic = &ceilCharacteristic;
-			break;
-		case 0xdf:
-			targetBuf = thermBuf;
-			targetCharacteristic = &thermCharacteristic;
-			break;
-		default:
-			led = 1;
-			return;
-	}
-	led = 0;
-
-	if (!targetCharacteristic)
-		return;
-
-	bool changed = false;
-	for (size_t i = 1; i < sizeof(serialBuf) / sizeof(*serialBuf); i++) {
-		if (targetBuf[i] != serialBuf[i]) {
-			targetBuf[i] = serialBuf[i];
-			changed = 1;
-		}
-	}
-
-	if (changed)
-		BLE::Instance().updateCharacteristicValue(targetCharacteristic->getValueAttribute().getHandle(), serialBuf + 1, sizeof(serialBuf) - 1);
-}
-
-void scheduleRead() {
-	uart.read(serialBuf, sizeof(serialBuf), &onRead);
-}
-
-Ticker stillAliveTicker;
-
-void onTick() {
-	//const char* buf = "hi\r\n";
-	//uart.write(reinterpret_cast<const uint8_t*>(buf), sizeof(buf));
-}
-
-
-void onConnection(const Gap::ConnectionCallbackParams_t* params) {
-}
-
-void onDisconnection(const Gap::DisconnectionCallbackParams_t* params) {
-	BLE::Instance().gap().startAdvertising();
-}
-
-
-void bleInitialized(BLE::InitializationCompleteCallbackContext* ctx) {
-	// if (ctx->error) {
-	// 	led = 1;
-	// 	return;
-	// }
-	// BLE &ble = BLE::Instance();
-
-	// ble.gap().onConnection(&onConnection);
-	// ble.gap().onDisconnection(&onDisconnection);
-
-	// ble.gattServer().addService(acService);
-	// ble.gap().setDeviceName(reinterpret_cast<const uint8_t*>(kDeviceName));
-	// ble.gap().startAdvertising();
-}
-
-#endif
-
 int main() {
-	//uart.format(8, SerialBase::Even);
-	//NRF_UART0->BAUDRATE = 0x21000;
-	//nrf_gpio_cfg_input(P0_30, NRF_GPIO_PIN_PULLUP);
-	//stillAliveTicker.attach(&onTick, 1);
-	//scheduleRead();
-
 	BLE &ble = BLE::Instance();
 	ble.init();
 
